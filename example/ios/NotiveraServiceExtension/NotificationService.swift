5e497866a2ed1eddfda714365d7965ff87bb04b7
razitiwana <razitiwana@notivera.com>
2026-08-30 14:22:27 +0500

    Update version to 5.0.1+10, modify iOS Info.plist for notification categories, and enhance NotificationServiceExtension to handle additional notification types.

--- example/ios/NotiveraServiceExtension/NotificationService.swift
+++ example/ios/NotiveraServiceExtension/NotificationService.swift
@@ -1,13 +1,16 @@
 import NotiveraSDK
+import UserNotifications
 
 class NotificationServiceExtension: NotiveraServiceExtension {
 
-    override func didReceive(
-        _ request: UNNotificationRequest,
-        withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void
-    ) {
-       if request.isNotiveraRequest {
-           super.didReceive(request, withContentHandler: contentHandler)
-       }
+  override func didReceive(
+    _ request: UNNotificationRequest,
+    withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void
+  ) {
+    if request.isNotiveraRequest {
+      super.didReceive(request, withContentHandler: contentHandler)
+    } else {
+      contentHandler(request.content)
     }
+  }
 }
